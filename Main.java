5c820f88006a1177434fc8b68a8775d1155d3d3f
JairomanExe <Jairoman777@gmail.com>
2016-05-24 14:12:16 -0700

    The main class and the Die class
    The die class emulates a 21 sided die, and stores the result in value.
    .getValue must be called to get the die value.

---
+++ Main.java
@@ -0,0 +1,27 @@
+/**
+ * @Description This is the main program that will work the magic
+ * 
+ * @author Jairo Chavez
+ * 
+ * @Date 5/24/16
+ *
+ */
+public class Main 
+{
+	public static void main (String args[])
+	{
+
+	// creates a die of type Die
+    Die die1;
+     
+     //my Die
+     die1= new Die();  	  
+     	 
+     
+     		//die1.roll();
+            System.out.println(die1.roll());
+            System.out.println();
+ 
+    }      
+}
+
